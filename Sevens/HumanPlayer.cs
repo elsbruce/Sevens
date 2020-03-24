6d7c00556fd1224bcf449b5074429bd0f2342f1b
Eleanor Bruce <K033994@eltham-college.org.uk>
2020-03-24 15:30:27 +0000

    neatening up unneeded code

--- Sevens/HumanPlayer.cs
+++ Sevens/HumanPlayer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Sevens
 { //when a card is hidden from gui, name isn't changed
     //when a card is removed from player hand, reference is changed therefore number is reduced
@@ -19,8 +14,6 @@ namespace Sevens
         {
             return null;
         }
-
-
         }
 
     }
